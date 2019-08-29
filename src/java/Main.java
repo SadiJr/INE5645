99c9e2575293b35aff2b2ca7a969fcc7174c3c68
Sadi <sadi.jacinto@assertsistemas.com.br>
2019-08-29 09:15:06 -0300

    Removing unnecessary I/O

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,13 +1,6 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 public class Main {
-	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-	
 	private static final String ERROR_MESSAGE = "Please type only numbers, idiot!\n";
 
 	private static int[][] arr;
@@ -19,6 +12,7 @@ public class Main {
 		int columns = treatUserInput("Expected size of columns: ", ERROR_MESSAGE, scan);
 		int threads = treatUserInput("Expected number of threads: ", ERROR_MESSAGE, scan);
 
+		/*
 		int processors = Runtime.getRuntime().availableProcessors();
 		if (threads > processors) {
 			LOGGER.info("The max numbers of processors in system (" + processors
@@ -26,22 +20,23 @@ public class Main {
 					+ "Changing the number of threads to max number of processors in this system...");
 			threads = processors;
 		}
+		*/
 		maxThreads = threads;
 
-		System.out.println("Generating random array...");
+		System.out.println("\nGenerating random matrix...");
 
 		arr = createRandom2dArray(rows, columns);
 		int[][] clone2dArray = clone2DArray(arr);
 
-		System.out.println("The random array generated is:\n");
-		printArray();
-		System.out.println();
+		System.out.println("The random matrix generated is:\n");
+		printMatrix();
 
 		long start = 0;
 		long end = 0;
+		System.out.println("\nExecuting Bubble Sort Algorithm...");
 		start = System.nanoTime();
 		for (int i = 0; i < threads; i++) {
-			Thread t = new Thread(createRunnable(i, true));
+			Thread t = new Thread(createRunnableBubble(i));
 			t.start();
 			try {
 				t.join();
@@ -50,22 +45,19 @@ public class Main {
 			}
 		}
 		end = System.nanoTime();
+		System.out.println("Time to proccess matrix using Bubble Sort: " + (end - start) / 1_000_000_000.0 + " seconds");
+		System.out.println("\nThe ordened matrix is:\n");
+		printMatrix();
 		System.out.println();
-		System.out.println("The ordened array is:\n");
-		printArray();
-		System.out.println();
-
-		System.out.println("Time to proccess Array using bubble sort: " + (end - start) / 1_000_000_000.0 + " seconds");
 
 		arr = clone2dArray;
-		System.out.println();
-		System.out.println("Restoring original array:\n");
-		printArray();
-		System.out.println();
-
+		System.out.println("Restoring original matrix... Restored matrix is: \n");
+		printMatrix();
+		
+		System.out.println("\nExecuting Merge Sort Algorithm...");
 		start = System.nanoTime();
 		for (int i = 0; i < threads; i++) {
-			Thread t = new Thread(createRunnable(i, false));
+			Thread t = new Thread(createRunnableMerge(i));
 			t.start();
 			try {
 				t.join();
@@ -74,10 +66,9 @@ public class Main {
 			}
 		}
 		end = System.nanoTime();
-		System.out.println();
-		System.out.println("The ordened array is:\n");
-		printArray();
-		System.out.println("\nTime to proccess Array using merge sort: " + (end - start) / 1_000_000_000.0 + " seconds");
+		System.out.println("Time to proccess matrix using merge sort: " + (end - start) / 1_000_000_000.0 + " seconds");
+		System.out.println("\nThe ordened matrix is:\n");
+		printMatrix();
 	}
 
 	private static int[][] createRandom2dArray(int rows, int columns) {
@@ -91,14 +82,12 @@ public class Main {
 		return arr;
 	}
 
-	private static Runnable createRunnable(int row, boolean b) {
-		return () -> {
-			if (b) {
-				bubble(row);
-			} else {
-				merge(row);
-			}
-		};
+	private static Runnable createRunnableBubble(int row) {
+		return () -> bubble(row);
+	}
+	
+	private static Runnable createRunnableMerge(int row) {
+		return () -> merge(row);
 	}
 
 	private static void bubble(int row) {
@@ -109,7 +98,7 @@ public class Main {
 				running = false;
 				continue;
 			}
-			System.out.println("Thread " + Thread.currentThread().getId() + " processing row " + row);
+			// System.out.println("Thread " + Thread.currentThread().getId() + " processing row " + row);
 
 			for (int j = 0; j < arr[0].length; j++) {
 				for (int k = 0; k < arr[0].length - 1; k++) {
@@ -131,7 +120,7 @@ public class Main {
 				running = false;
 				continue;
 			}
-			System.out.println("Thread " + Thread.currentThread().getId() + " processing row " + row);
+			// System.out.println("Thread " + Thread.currentThread().getId() + " processing row " + row);
 			
 			mergeSort(row, 0, arr[0].length - 1);
 			row += maxThreads;
@@ -186,7 +175,7 @@ public class Main {
 		return input;
 	}
 
-	private static void printArray() {
+	private static void printMatrix() {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
 				System.out.print(arr[i][j] + " ");
