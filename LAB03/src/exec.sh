8fb9ee3168905096da9f529fc065c4761cee53d7
SadiJr <sadijacinto@gmail.com>
2019-11-23 21:59:05 -0300

    Changing tests

--- LAB03/src/exec.sh
+++ LAB03/src/exec.sh
@@ -8,10 +8,12 @@ if [ ! -d "process" ]; then
 	mpicc -o process mpi_process_priority.c
 fi
 
-for i in "memory" "process"; do
-	for j in {1..8}; do
-		for k in 10 100 1000 10000 100000 1000000 10000000; do
-			mpirun -np "$j" "$i" "$k" &>> "$i"_results.txt 
+for h in {1..20}; do
+	for i in "memory" "process"; do
+		for j in {1..16}; do
+			for k in 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000 10000000000; do
+				mpirun --use-hwthread-cpus -np "$j" "$i" "$k" &>> "$i"_results.txt 
+			done
 		done
 	done
 done
